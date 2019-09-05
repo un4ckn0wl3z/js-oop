bd7ff5fedef99b7575a2cf87bbac60b34860b00f
un4ckn0wl3z <slickerth@gmail.com>
2019-09-05 21:58:35 +0700

    Function()

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let x = {};
-new String();
-new Boolean();
-new Number();
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
 
+const circle = new Circle(1);
+circle.draw();
+
+const Circle1 = new Function('radius', `
+this.radius = radius;
+this.draw = function(){
+    console.log('draw');
+}
+`);
+
+const circle1 = new Circle1(1);
