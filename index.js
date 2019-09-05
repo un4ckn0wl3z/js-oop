b1673d3d4ef6bf0f0981499ba4ac36423611857d
un4ckn0wl3z <slickerth@gmail.com>
2019-09-05 21:50:14 +0700

    add constructor

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function createCircle(radius) {
 
 const circle = createCircle(1);
 circle.draw();
+
+// constructor function
+function Circle(radius){
+    console.log('this',this);
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1);
+another.draw();
