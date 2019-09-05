98899e26726a482e71f08a0d210dd224c4396073
un4ckn0wl3z <slickerth@gmail.com>
2019-09-05 22:01:28 +0700

    javascript function are object

--- index.js
+++ index.js
@@ -8,11 +8,5 @@ function Circle(radius){
 const circle = new Circle(1);
 circle.draw();
 
-const Circle1 = new Function('radius', `
-this.radius = radius;
-this.draw = function(){
-    console.log('draw');
-}
-`);
-
-const circle1 = new Circle1(1);
+Circle.call({}, 1);
+Circle.apply({}, [1]);
