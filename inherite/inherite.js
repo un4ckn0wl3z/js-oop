fb0f741b5ba308e5ae32d03cd4259a95ea776763
un4ckn0wl3z <slickerth@gmail.com>
2019-09-07 21:23:04 +0700

    035 4- Intermediate Function Inheritance

--- inherite/inherite.js
+++ inherite/inherite.js
@@ -1,22 +1,31 @@
-function Shape(color){
-    this.color = color;
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
 }
 
+function Shape(color) {
+    this.color = color;
+}
 Shape.prototype.duplicate = function () {
     console.log('duplicate');
 }
-
 function Circle(radius, color) {
     Shape.call(this, color);
     this.radius = radius;
 }
+extend(Circle, Shape);
 
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
 
 Circle.prototype.draw = function () {
     console.log('draw');
 }
+
+function Square(size) {
+    this.size = size;
+}
+extend(Square, Shape);
+
+
 
 const s = new Shape();
 const c = new Circle(1, 'red');
