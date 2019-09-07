612cb57c85be0772639da11bc70e73dcf7bf3d06
un4ckn0wl3z <slickerth@gmail.com>
2019-09-07 20:58:55 +0700

    032 1- Creating Your Own Prototypical Inheritance

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 function Circle(radius) {
-    // instance members
     this.radius = radius;
-    this.move = function () {
-        // this.draw();
-        console.log('move');
-    }
 }
 
-// prototype members
-Circle.prototype.toString = function () {
-    return 'Circle with radius ' + this.radius;
-}
 Circle.prototype.draw = function () {
-    this.move();
     console.log('draw');
 }
 
-const c1 = new Circle(1);
-c1.draw();
-
-console.log(Object.keys(c1)); // return instance member
-
-for (let key in c1) {
-    console.log(key); // return all both from instance member and prototype
-
+Circle.prototype.duplicate = function () {
+    console.log('duplicate');
 }
