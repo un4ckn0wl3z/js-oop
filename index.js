4b2af874a15e77f3ed445b5bf5b3761dec27566d
un4ckn0wl3z <slickerth@gmail.com>
2019-09-07 20:25:00 +0700

    Iterating Instance and Prototype Members-en

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// 026 6- Prototype vs. Instance Members
-
 function Circle(radius) {
     // instance members
     this.radius = radius;
@@ -10,14 +8,20 @@ function Circle(radius) {
 }
 
 // prototype members
+Circle.prototype.toString = function () {
+    return 'Circle with radius ' + this.radius;
+}
 Circle.prototype.draw = function () {
     this.move();
     console.log('draw');
 }
 
-Circle.prototype.toString = function () {
-    return 'Circle with radius ' + this.radius;
-}
-
 const c1 = new Circle(1);
-const c2 = new Circle(1);
+c1.draw();
+
+console.log(Object.keys(c1)); // return instance member
+
+for (let key in c1) {
+    console.log(key); // return all both from instance member and prototype
+
+}
