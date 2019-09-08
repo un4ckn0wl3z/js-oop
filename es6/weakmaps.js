46806cb79202082a523cd147c1d678e5b1e5afd6
un4ckn0wl3z <slickerth@gmail.com>
2019-09-08 10:04:46 +0700

    weak map

--- es6/weakmaps.js
+++ es6/weakmaps.js
@@ -1,9 +1,18 @@
 const _radius = new WeakMap();
-
+const _move = new WeakMap();
 
 class Circle {
     constructor(radius) {
-        this.radius = radius;
-
+        _radius.set(this, radius);
+        _move.set(this, () => {
+            console.log('move', this);
+        });
+    }
+    draw(){
+        console.log(_radius.get(this));
+        _move.get(this)();
+        console.log('draw');
     }
 }
+
+const c = new Circle(1);
